d025278fa4c52b93dd5aa539d9994494b3c88ccf
Jinyan Yang <jyyoung@live.com>
2021-11-02 16:49:23 +1100

    main file

---
+++ main.R
@@ -0,0 +1,20 @@
+#Welcome to grassland phenology modlling project#####################################
+# thie repo fits the Choler-Hufkens to observed cover################################
+#By Jinyan Yang (jyyoung@live.com)###################################################
+
+# known issues####
+# 1. I have not yet made the dependent packages install automatically;
+# 2. fitting take a long time so may as well just use the fitted reasults 
+# (i.e., skip step II in the following);
+
+
+# Step I: load environment####
+source('r/read_spc_nm.R')
+
+# Step II: fit model to data####
+source('r/fit_all.R')
+
+# Step III: make plots#####
+source('makeFigure/plot_fit.R')
+
+
