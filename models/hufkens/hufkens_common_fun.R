d316df42ef0b838c20df15917a3f6272a78cee9f
Jinyan Yang <jyyoung@live.com>
2022-01-08 22:48:35 +1100

    new t from sgs

--- models/hufkens/hufkens_common_fun.R
+++ models/hufkens/hufkens_common_fun.R
@@ -69,17 +69,35 @@ scaling.f.func <- function(map,f.h){
 t.func <- function(t.mean,f.t.opt,t.max){
   # need to determine which is the best way#
   
-  # #1.ch tyope of t dependence
+  # #1.ch tyope of t dependence####
   # return((t.max-t.mean)/(t.max-f.t.opt)*(t.mean/f.t.opt)^(f.t.opt/(t.max-f.t.opt)))
   
-  # #2. p based sharp decline
+  # #2. p based sharp decline####
   # h.val <- pnorm(t.mean,mean=f.t.opt,sd = 15,lower.tail = F)
   # l.val <- pnorm(t.mean,mean=f.t.opt,sd = 15,lower.tail = T)
   # return(min(h.val,l.val)*2)
   
-  # #3. sin function 
-  f.t <- sin(t.mean/f.t.opt * pi/2)
-  return(max(0,f.t))
+  # #3. sin function ####
+  # frac <- t.mean/f.t.opt
+  # # make sure sin function stay with the singal sin range
+  # frac <- min(max(0,frac),2)
+  # 
+  # f.t <- sin(frac * pi/2)
+  # return(max(0,f.t))
+  
+  # 4. sgs based T response#####
+  # mimic the peak arrhenius curve
+  # this function is based on Thornley (1998)
+  q <- 2.5#power or sensitivity of the function
+  t.mn <- 5#MIN t FOR GROWTH; 5 degree seems reasonable?
+
+  # here we assume that Topt and 
+  # Tr (the T at which function is 1) to be the same 
+  # so that the function is from 0 to 1
+  frac <- ((t.mean - t.mn)/(f.t.opt - t.mn))^q *
+    ((1+q)*f.t.opt - t.mn - q*t.mean) /
+    ((1+q)*f.t.opt - t.mn - q*f.t.opt)
+  return(max(0,frac))
 }
 
 # drainage
