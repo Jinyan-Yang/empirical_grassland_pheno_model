2bd4f9a12b96e6c780a57953c3bc5538b35037ed
JinyanYang <jyyoung@live.com>
2022-02-04 10:30:40 +1100

    fig

--- r/read_spc_nm.R
+++ r/read_spc_nm.R
@@ -1,5 +1,5 @@
 
-species.vec <- c('Bis','Luc','Dig','Kan','Rho','Fes','Pha','Rye','ym','flux')[8:10]#[2:10]
+species.vec <- c('Bis','Luc','Dig','Kan','Rho','Fes','Pha','Rye','ym','flux')#[9:10]#[2:10]
 
 species.vec.nm <- c('Bis','Med','Dig','The','Chl','Fes','Pha','Lol','YM','FT')
 
